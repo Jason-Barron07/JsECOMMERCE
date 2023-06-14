87cd0671f51a3315ad8565128b7dcca0623ec6c8
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-14 14:05:30 +0200

    form

--- script/Index.js
+++ script/Index.js
@@ -1,6 +1,6 @@
 let content = document.querySelector('body').innerHTML +=`
 
-<nav class="navbar navbar-expand-lg ">
+  <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
           <a class="navbar-brand" href="#"><h1 class="head-text"><span>SN3</span>AKER <span>Com</span>plex </h1></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,10 +21,6 @@ let content = document.querySelector('body').innerHTML +=`
               <li class="nav-item">
                 <a class="nav-link active" href="Admin.html"><h4>Admin</h4></a>
               </li>
-
-              <li class="nav-item">
-                <a class="nav-link active" href="Checkout.html"><h4>Checkout</h4></a>
-              </li>
               
             </ul>
 
@@ -32,27 +28,6 @@ let content = document.querySelector('body').innerHTML +=`
         </div>
 
        
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
         
       </nav>
 
@@ -70,6 +45,10 @@ let content = document.querySelector('body').innerHTML +=`
 
     <button type="button" class="btn button" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Contact Us</button>
 
+    <form
+action="https://formspree.io/f/xnqyrary"
+method="POST"
+>
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -85,7 +64,7 @@ let content = document.querySelector('body').innerHTML +=`
           </div>
           <div class="mb-3">
           <label for="recipient-name" class="col-form-label">Surname:</label>
-          <input type="text" class="form-control" id="recipient-name">
+          <input type="text" class="form-control" id="recipient-name" name="surname">
         </div>
         <div class="mb-3">
         <label for="recipient-name" class="col-form-label">Gmail:</label>
@@ -99,13 +78,14 @@ let content = document.querySelector('body').innerHTML +=`
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
+        <button type="submit" class="btn btn-primary" >Send message</button>
       </div>
     </div>
   </div>
 </div>
 
-    
+
+</form>
 
 <footer>
 
